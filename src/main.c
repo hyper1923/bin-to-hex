cbd08718f490eca28b281f1056d1ef094fbcbcc9
hyper1923 <hyperamabanyiyen@gmail.com>
2022-09-24 11:43:10 +0300

    push

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ void start_dumping(char* argv1,char* argv2){
     FILE* f = fopen(argv1,"r");
     if(!f){printf("\nFile not found\n"); return;}
     unsigned char *data = (char*)malloc(1024*32);
-    int i = 0;
+    unsigned int i = 0;
     while(!feof(f)){
         data[i] = fgetc(f);
         i++;
@@ -16,11 +16,13 @@ void start_dumping(char* argv1,char* argv2){
     fclose(f);
     f = fopen(argv2, "w");
     i--;
+    unsigned int g = 0;
     if(!f){printf("\nFile not found\n"); return;}
     for(int j = 0; j < i; j++){
         if(data[j] != '\0' || data[j] != '\n') fprintf(f,"0x%02x", data[j]);
         if(j != i-1) fputc(',',f);
-        if(j != i-1) fputc('\n',f);
+        g++;
+        if(g >= 6) {fputc('\n',f), g = 0;};
     }
     fclose(f);
     printf("\nOperation finished successfully\n");
